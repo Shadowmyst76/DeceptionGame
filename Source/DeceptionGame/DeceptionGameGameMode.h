d474acef15b9327fd95c5bc720bd21bb90d1f5ac
Shadowmyst76 <Shadowassassin76@gmail.com>
2018-03-07 18:13:07 -0700

    Initial commit

---
+++ Source/DeceptionGame/DeceptionGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DeceptionGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADeceptionGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADeceptionGameGameMode();
+};
+
+
+
